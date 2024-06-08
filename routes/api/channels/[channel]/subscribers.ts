2540cb6894045ec8242e034e799a144041c48b1f
Alexandre Negrel <alexandre@negrel.dev>
2024-06-08 22:33:20 +0200

    add API endpoint to delete subscription

--- routes/api/channels/[channel]/subscribers.ts
+++ routes/api/channels/[channel]/subscribers.ts
@@ -1,17 +1,34 @@
-import { Handler } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
 import { State } from "@/routes/_middleware.ts";
 import { PushSubscription } from "@negrel/webpush";
 
 // deno-lint-ignore no-explicit-any
-export const handler: Handler<any, State> = async (req, ctx) => {
-  const subscription: PushSubscription = await req.json();
-  const channel = ctx.params["channel"];
-  const hostname = ctx.url.hostname;
+export const handler: Handlers<any, State> = {
+  async POST(req, ctx) {
+    const subscription: PushSubscription = await req.json();
+    const channel = ctx.params["channel"];
+    const hostname = ctx.url.hostname;
 
-  ctx.state.kv.set(
-    [hostname, "subscriptions", channel, crypto.randomUUID()],
-    subscription,
-  );
+    ctx.state.kv.set(
+      [hostname, "subscriptions", channel, subscription.endpoint],
+      subscription,
+    );
 
-  return new Response();
+    return new Response();
+  },
+
+  async DELETE(req, ctx) {
+    const subscription: PushSubscription = await req.json();
+    const channel = ctx.params["channel"];
+    const hostname = ctx.url.hostname;
+
+    await ctx.state.kv.delete([
+      hostname,
+      "subscriptions",
+      channel,
+      subscription.endpoint,
+    ]);
+
+    return new Response();
+  },
 };
